2c02e42c0a57a9bfeda8dd93a2c0ff4d07d0901d
Aldo Almanzar <ara4143@rit.edu>
2025-01-27 07:03:15 -0500

    Update WeatherCard files and image

---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,14 @@
+import sunny from "../../images/sunny.png"
+
+function WeatherCard() {
+    return (
+    <section className="weather-card">
+        <p className="weather-card__temp">75 &deg; F</p>
+        <img src={sunny} alt="sunny" className="weather-card__iimage" />
+    </section>
+    );
+}
+
+
+
+export default WeatherCard;
