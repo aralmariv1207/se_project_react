292e8f7a845727595326cf5f1bb194b2c6b9543a
Aldo Almanzar <ara4143@rit.edu>
2025-01-28 13:51:37 -0500

    Update: 'Main.jsx' file

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import "./Main.css";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 import { defaultClothingItems } from "../../utils/constants";
+import ItemCard from "../ItemCard/ItemCard";
 
 function Main({ weatherData, handleCardClick }) {
     return (
