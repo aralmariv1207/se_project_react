773da0dce577be02a2defa6c9a8d942574cb33a2
aralmariv1207 <aralmariv1207@users.noreply.github.com>
2025-03-10 13:47:32 -0400

    Update: 'ItemModal.jsx' file

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -11,6 +11,7 @@ function ItemModal({ activeModal, onClose, card }) {
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
           <p className="modal__weather">Weather: {card.weather}</p>
+          <button>Delete Item</button>
         </div>
       </div>
     </div>
