86463db8eff82cb8584d44be70469ef03362b521
aralmariv1207 <aralmariv1207@users.noreply.github.com>
2025-03-10 16:51:45 -0400

    Update: 'ItemModal.jsx' and 'ItemModal.css' files

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -9,8 +9,10 @@ function ItemModal({ activeModal, onClose, card }) {
         </button>
         <img src={card.imageUrl} alt={card.name} className="modal__image" />
         <div className="modal__footer">
+          <div>
           <h2 className="modal__caption">{card.name}</h2>
           <p className="modal__weather">Weather: {card.weather}</p>
+          </div>
           <button className="modal__garment-type">Delete Item</button>
         </div>
       </div>
