073b737ab471bb413eab1ecea435a9be1d7b0220
aralmariv1207 <aralmariv1207@users.noreply.github.com>
2025-03-12 15:19:44 -0400

    Update 'ConfirmDeleteModal.jsx' file

--- src/components/ConfirmDeleteModal/ConfirmDeleteModal.jsx
+++ src/components/ConfirmDeleteModal/ConfirmDeleteModal.jsx
@@ -1,7 +1,23 @@
 import 'ConfirmDeleteModal.css';
 
 function ConfirmDeleteModal({ handleCardDelete, handleClothing }) {
-    
+  return (
+    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+      <div className="modal__content modal__content_type_image">
+        <button onClick={onClose} type="button" className="modal__close modal__close_type_image">
+        
+        </button>
+        <img src={card.imageUrl} alt={card.name} className="modal__image" />
+        <div className="modal__footer">
+          <div>
+          <h2 className="modal__caption">{card.name}</h2>
+          <p className="modal__weather">Weather: {card.weather}</p>
+          </div>
+          <button className="modal__garment-type" onClick={() => openDeleteModal(card)}>Delete item</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 const handleDeleteConfirm = (itemId) => {
