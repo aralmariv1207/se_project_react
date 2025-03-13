aab59d3d172d1e92d4c2afc0a84f5a89ea6bc44d
aralmariv1207 <aralmariv1207@users.noreply.github.com>
2025-03-12 23:02:16 -0400

    Update 'api.js' file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 const baseUrl = "http://localhost:3001";
 
-function getItems() {
+export function getItems() {
   return fetch(`${baseUrl}/items`).then((res) => {
     return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
   });
