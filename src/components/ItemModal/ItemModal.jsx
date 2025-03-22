83eb84b55e20b434052dad81dd1d5350745cfd87
aralmariv1207 <aralmariv1207@users.noreply.github.com>
2025-03-22 19:04:43 -0400

    Update: 'ItemModal.jsx' and 'Modal.css' files

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -4,13 +4,17 @@ import { Modal } from "../Modal/Modal.jsx";
 function ItemModal({ activeModal, onClose, card, openDeleteModal }) {
   return (
     activeModal === "preview" && (
-      <Modal onClose={onClose}>
+      <Modal onClose={onClose} isOpen={activeModal === "preview"}>
         <div className="modal__content_type_image">
           <img src={card.imageUrl} alt={card.name} className="modal__image" />
           <div className="modal__footer">
             <p className="modal__caption">{card.name}</p>
             <p className="modal__weather">Weather: {card.weather}</p>
-            <button className="modal__close" type="button" onClick={onClose}>
+            <button
+              className="modal__close"
+              type="button"
+              onClick={openDeleteModal}
+            >
               Delete item
             </button>
           </div>
