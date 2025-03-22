108efe62636abbf20c1a2ac8258c1b83283c60b0
aralmariv1207 <aralmariv1207@users.noreply.github.com>
2025-03-22 03:40:53 -0400

    Update: 'api.js' file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,22 @@
 const baseUrl = "http://localhost:3001";
 
-export function getItems() {
-  return fetch(`${baseUrl}/items`).then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-  });
-}
+const checkResponse = (res) => {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error: ${res.status}`);
+};
+
+const request = (url, options) => {
+  return fetch(url, options).then(checkResponse);
+};
+
+export const getItems = () => {
+  return request(`${baseUrl}/items`);
+};
 
 export const addItem = ({ name, weather, imageUrl }) => {
-  return fetch(`${baseUrl}/items`, {
+  return request(`${baseUrl}/items`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -17,18 +26,11 @@ export const addItem = ({ name, weather, imageUrl }) => {
       weather,
       imageUrl,
     }),
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Error: ${res.status}`);
   });
 };
 
 export const deleteItem = (id) => {
-  return fetch(`${baseUrl}/items/${id}`, {
+  return request(`${baseUrl}/items/${id}`, {
     method: "DELETE",
-  }).then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
   });
 };
