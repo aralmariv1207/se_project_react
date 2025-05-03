fa7be8157677c34d010957884660c552411c644c
aralmariv1207 <almariv.ar1207@outlook.com>
2025-05-02 22:39:02 -0400

    Update: 'LoginModal.jsx' file

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,13 +1,8 @@
+import { useState } from "react";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import useForm from "../../hooks/useForm";
 
-function LoginModal({
-  isOpen,
-  onClose,
-  onSubmit,
-  errorMessage,
-  onClickRegister,
-}) {
+function LoginModal({ isOpen, onClose, onSubmit, onClickRegister }) {
   const { values, errors, handleChange, setValues } = useForm({
     email: "",
     password: "",
@@ -21,6 +16,19 @@ function LoginModal({
   const handleClose = () => {
     setValues({ email: "", password: "" });
     onClose();
+  };
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleError = (e) => {
+    const value = e.target.value;
+    const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+
+    if (!emailRegex.test(value)) {
+      setErrorMessage("Please enter a valid email address.");
+    } else {
+      setErrorMessage("");
+    }
   };
 
   return (
@@ -43,6 +51,7 @@ function LoginModal({
           value={values.email}
           onChange={handleChange}
           placeholder="Email"
+          onBlur={handleError}
           required
         />
         {errors.email && <p className="modal__error">{errors.email}</p>}
@@ -57,6 +66,7 @@ function LoginModal({
           value={values.password}
           onChange={handleChange}
           placeholder="Password"
+          onBlur={handleError}
           required
         />
         {errors.password && <p className="modal__error">{errors.password}</p>}
