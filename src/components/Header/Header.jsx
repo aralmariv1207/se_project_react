8477114184e15e0e42b19f3a234f15d96b5618f7
aralmariv1207 <almariv.ar1207@outlook.com>
2025-05-13 00:29:13 -0400

    Update: 'Header.jsx' and 'Header.css' files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,13 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 
-function Header({ weatherData, onLoginClick, onRegisterClick, onAddNewItem }) {
+function Header({
+  weatherData,
+  onLoginClick,
+  onRegisterClick,
+  onAddNewItem,
+  isLoggedIn,
+}) {
   const { currentUser } = useContext(CurrentUserContext);
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
@@ -14,42 +20,51 @@ function Header({ weatherData, onLoginClick, onRegisterClick, onAddNewItem }) {
 
   return (
     <header className="header">
-      <Link to="/">
-        <img className="header__logo" src={logo} alt="logo" />
-      </Link>
-      <p className="header__date-and-location">
-        {currentDate}, {weatherData.city}
-      </p>
-      <ToggleSwitch />
-      {currentUser ? (
-        <div className="header__user-info">
-          <button className="header__button" onClick={onAddNewItem}>
-            + Add clothes
-          </button>
-          <Link to="/profile" className="header__button">
-            {currentUser.name}
-            <div className="header__avatar">
-              {currentUser.avatar ? (
-                <img
-                  src={currentUser.avatar}
-                  className="header__avatar-img"
-                  alt="avatar"
-                />
-              ) : (
-                currentUser.name.charAt(0).toUpperCase()
-              )}
-            </div>
-          </Link>
-        </div>
-      ) : null}
-      {currentUser ? (
-        <div className="header__actions"></div>
-      ) : (
-        <div className="header__auth-buttons">
-          <button onClick={onLoginClick}>Log in</button>
-          <button onClick={onRegisterClick}>Sign up</button>
-        </div>
-      )}
+      <div className="header__container">
+        <Link to="/">
+          <img src={logo} alt="WTWR Logo" className="header__logo" />
+        </Link>
+        <p className="header__date">
+          {currentDate}, {weatherData.city}
+        </p>
+      </div>
+      <nav className="navigation">
+        <ul className="navigation__container">
+          </ul>
+          <ToggleSwitch />
+          <li>
+            <button className="header__button" onClick={onAddNewItem}>
+              + Add clothes
+            </button>
+          </li>
+          <li>
+            <Link to="/profile" className="header__button">
+              {isLoggedIn.name}
+              <div className="header__avatar">
+                {currentUser.avatar ? (
+                  <img
+                    src={currentUser.avatar}
+                    className="header__avatar-img"
+                    alt="avatar"
+                  />
+                ) : (
+                  currentUser.name.charAt(0).toUpperCase()
+                )}
+              </div>
+            </Link>
+          </li>
+        </ul>
+        ) : (
+        <ul className="navigation__container">
+          <li>
+            <button onClick={onLoginClick}>Log in</button>
+          </li>
+          <li>
+            <button onClick={onRegisterClick}>Sign up</button>
+          </li>
+        </ul>
+        )}
+      </nav>
     </header>
   );
 }
